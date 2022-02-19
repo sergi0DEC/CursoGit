b91508aac6f0f6abe2dd4444ad676a581ba875de
Sergio Escamilla <95187364+sergi0DEC@users.noreply.github.com>
2022-02-18 21:03:29 -0600

    Update modulos_disenio.py

--- modulos_disenio.py
+++ modulos_disenio.py
@@ -3,3 +3,7 @@ def pantalla():
 
 def segundoDisenio():
     print("Implementando segundo disenio")
+    
+#Diseño agregador desde GitHub
+def DisenioGitHub():
+    print("Wow, realmente implemente un diseño desde GitHub")
